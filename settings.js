c70c1944ffb73f09bccd89fdd132ed0b277e1267
CBStudio <47882601+CBStudio@users.noreply.github.com>
2020-02-03 17:54:53 +0200

    flow update

---
+++ settings.js
@@ -0,0 +1,14 @@
+module.exports = {
+     uiPort: 1880,
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000,
+    debugMaxLength: 1000,
+    flowFile: 'flows.json',
+    flowFilePretty: true,
+    disableEditor: false,
+    editorTheme: {
+        projects: {
+            enabled: false
+        }
+    },
+}
